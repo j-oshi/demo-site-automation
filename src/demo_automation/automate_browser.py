4e46e21742fa41579d5e67d5dadc9f0bfdb87ba6
j-oshi <jtoshin@msn.com>
2023-04-22 18:50:02 +0100

    add go to mortgage

--- src/demo_automation/automate_browser.py
+++ src/demo_automation/automate_browser.py
@@ -13,10 +13,6 @@ import os, sys
 
 base_url = 'http://demo.oshinit.com/'
 
-# parent_dir = os.path.abspath('..')
-# if parent_dir not in sys.path:
-#     sys.path.append(parent_dir)
-
 def autoWebpage():
     try:
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -24,13 +20,10 @@ def autoWebpage():
         driver.get(base_url)
         wait = WebDriverWait(driver, 10)
         time.sleep(4)
-#     #     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, css_class)))
-#     #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#     #     time.sleep(6)
-#     #     result = bs(driver.page_source, 'lxml')
-#     # except NoSuchElementException:
-#     #     print("Error finding element")
-#     # except ConnectionError:
-#     #     print("No connection")
-    except:
-        print("Something else went wrong") 
+        wait.until(EC.presence_of_element_located((By.TAG_NAME, 'a')))
+        link = driver.find_element(By.PARTIAL_LINK_TEXT, 'Mortgage')
+        link.click()
+        time.sleep(6)
+        wait.until(EC.presence_of_element_located((By.TAG_NAME, 'h1')))
+        time.sleep(6)
+    except Exception as e: print(e)
